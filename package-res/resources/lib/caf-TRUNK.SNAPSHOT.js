40383110995163ec751fd9f2986ed0c27ff492e1
Goncalo Neto <goncalo.neto@webdetails.pt>
2012-03-06 15:01:14 +0000

    Updated CAF build.

--- package-res/resources/lib/caf-TRUNK.SNAPSHOT.js
+++ package-res/resources/lib/caf-TRUNK.SNAPSHOT.js
@@ -32,6 +32,50 @@ if (!Array.prototype.map)
 }
 
 /** 
+ * Define Array indexOf function. Taken from MDN.
+ * @global
+ * @param {Object} searchElement - element to find
+ * @param {int} fromIndex
+ *
+ */
+
+if (!Array.prototype.indexOf) {  
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {  
+        "use strict";  
+        if (this == null) {  
+            throw new TypeError();  
+        }  
+        var t = Object(this);  
+        var len = t.length >>> 0;  
+        if (len === 0) {  
+            return -1;  
+        }  
+        var n = 0;  
+        if (arguments.length > 0) {  
+            n = Number(arguments[1]);  
+            if (n != n) { // shortcut for verifying if it's NaN  
+                n = 0;  
+            } else if (n != 0 && n != Infinity && n != -Infinity) {  
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));  
+            }  
+        }  
+        if (n >= len) {  
+            return -1;  
+        }  
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);  
+        for (; k < len; k++) {  
+            if (k in t && t[k] === searchElement) {  
+                return k;  
+            }  
+        }  
+        return -1;  
+    }  
+}
+
+
+
+
+/** 
  * Utility function to add a method to a function's prototype
  * @global
  * @param {string} name - method name

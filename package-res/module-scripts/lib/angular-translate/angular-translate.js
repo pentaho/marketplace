44277a336a1bf22e5fc66e2cbf12a611624dae5c
Guilherme Raimundo <guilherme.raimundo@webdetails.pt>
2014-09-02 19:48:06 +0100

    [SERVDEV-49] - Added polyfills for IE8 support

--- package-res/module-scripts/lib/angular-translate/angular-translate.js
+++ package-res/module-scripts/lib/angular-translate/angular-translate.js
@@ -3,6 +3,52 @@
  * http://github.com/PascalPrecht/angular-translate
  * Copyright (c) 2014 ; Licensed MIT
  */
+
+/**
+ * IE8 Polyfills
+ */
+
+// Add trim function to support IE8
+if(typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, '');
+  };
+}
+
+// Add indexOf function to support IE8
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError();
+    }
+    var n, k, t = Object(this),
+        len = t.length >>> 0;
+
+    if (len === 0) {
+      return -1;
+    }
+    n = 0;
+    if (arguments.length > 1) {
+      n = Number(arguments[1]);
+      if (n != n) { // shortcut for verifying if it's NaN
+        n = 0;
+      } else if (n != 0 && n != Infinity && n != -Infinity) {
+        n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+    if (n >= len) {
+      return -1;
+    }
+    for (k = n >= 0 ? n : Math.max(len - Math.abs(n), 0); k < len; k++) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  };
+}
+
 angular.module('pascalprecht.translate', ['ng']).run([
   '$translate',
   function ($translate) {

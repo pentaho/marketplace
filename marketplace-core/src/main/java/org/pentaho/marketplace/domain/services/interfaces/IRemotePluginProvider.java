47f96899616f3f4af10ab166938536c6d8e61cfc
Guilherme Raimundo <guilherme.raimundo@webdetails.pt>
2015-03-17 17:17:42 +0000

    [CLEANUP] - Updated copyright headers

--- marketplace-core/src/main/java/org/pentaho/marketplace/domain/services/interfaces/IRemotePluginProvider.java
+++ marketplace-core/src/main/java/org/pentaho/marketplace/domain/services/interfaces/IRemotePluginProvider.java
@@ -1,14 +1,18 @@
 /*
- * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
  *
- * This software was developed by Webdetails and is provided under the terms
- * of the Mozilla Public License, Version 2.0, or any later version. You may not use
- * this file except in compliance with the license. If you need a copy of the license,
- * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Software distributed under the Mozilla Public License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
- * the license for the specific language governing your rights and limitations.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2015 Pentaho Corporation. All rights reserved.
  */
 
 package org.pentaho.marketplace.domain.services.interfaces;
--- marketplace-core/src/main/java/org/pentaho/marketplace/endpoints/MarketplaceContentGenerator.java
+++ marketplace-core/src/main/java/org/pentaho/marketplace/endpoints/MarketplaceContentGenerator.java
@@ -1,34 +1,18 @@
-/*!
-* Copyright 2002 - 2013 Pentaho Corporation.  All rights reserved.
-* 
-* This software was developed by Pentaho Corporation and is provided under the terms
-* of the Mozilla Public License, Version 2.0, or any later version. You may not use
-* this file except in compliance with the license. If you need a copy of the license,
-* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Pentaho Corporation.
-*
-* Software distributed under the Mozilla Public License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
-* the license for the specific language governing your rights and limitations.
-*/
-
 /*
- * This program is free software; you can redistribute it and/or modify it under the 
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
  * Foundation.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this 
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
- * or from the Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright 2013 Pentaho Corporation.  All rights reserved.
- *
- * Created Set 17th, 2012
- * @author Pedro Alves (pedro.alves@webdetails.pt)
+ * Copyright (c) 2015 Pentaho Corporation. All rights reserved.
  */
 
 package org.pentaho.marketplace.endpoints;

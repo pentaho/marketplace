97eb9a7a7868c49b85500fe6aa8606daa6c05277
Marco Vala <marco.vala@webdetails.pt>
2015-04-10 01:40:42 +0100

    [MARKET-33] - Unified telemetry service configuration for OSGi and non-OSGi plugins

--- marketplace-ba/src/main/java/org/pentaho/telemetry/BaPluginTelemetry.java
+++ marketplace-ba/src/main/java/org/pentaho/telemetry/BaPluginTelemetry.java
@@ -1,26 +1,25 @@
-/*!
-* This program is free software; you can redistribute it and/or modify it under the
-* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
-* Foundation.
-*
-* You should have received a copy of the GNU Lesser General Public License along with this
-* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-* or from the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See the GNU Lesser General Public License for more details.
-*
-* Copyright (c) 2002-2015 Pentaho Corporation. All rights reserved.
-*/
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2015 Pentaho Corporation. All rights reserved.
+ */
 
 package org.pentaho.telemetry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.pentaho.platform.api.engine.IApplicationContext;
+import org.osgi.framework.Bundle;
 import org.pentaho.platform.api.util.IVersionHelper;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.util.VersionHelper;
@@ -28,34 +27,83 @@ import org.pentaho.platform.util.VersionInfo;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
+
+/**
+ * Creates a telemetry service for a specific BA plugin (supports both OSGi or non-OSGi plugins).
+ */
 public class BaPluginTelemetry extends TelemetryService {
 
-  private static Log logger = LogFactory.getLog(BaPluginTelemetry.class);
+  private static Log logger = LogFactory.getLog( BaPluginTelemetry.class );
+  private static String UNKNOWN = "unknown";
 
-  // region Constructors
-
-  public BaPluginTelemetry(String pluginName,
-                           ITelemetryHandler telemetryHandler,
-                           String telemetryUrl,
-                           boolean telemetryEnabled) {
-    super(pluginName, telemetryUrl, telemetryEnabled, telemetryHandler);
-
-    this.setPluginVersion( this.getBaPluginVersion(pluginName) );
-    this.setPlatformVersion( this.getBaPlatformVersion() );
+  public BaPluginTelemetry( String pluginName,
+                            String telemetryUrl,
+                            boolean telemetryEnabled,
+                            ITelemetryHandler telemetryHandler ) {
+    super( pluginName, getPluginVersion( pluginName ), getBaVersion(), telemetryUrl, telemetryEnabled,
+      telemetryHandler );
   }
 
-  // endregion
+  public BaPluginTelemetry( Bundle bundle,
+                            String telemetryUrl,
+                            boolean telemetryEnabled,
+                            ITelemetryHandler telemetryHandler ) {
+    super( bundle.getSymbolicName(), getPluginVersion( bundle ), getBaVersion(), telemetryUrl, telemetryEnabled,
+      telemetryHandler );
+  }
 
-  // region Methods
+  private static String getPluginVersion( String pluginName ) {
+    try {
+      String versionPath =
+        PentahoSystem.getApplicationContext().getSolutionPath( "system/" + pluginName + "/version.xml" );
+      InputStream versionFile = new FileInputStream( new File( versionPath ) );
+      return parsePluginVersion( versionFile );
+    } catch ( FileNotFoundException fnfe ) {
+      logger.warn( "Could not find file version.xml", fnfe );
+    }
+    return UNKNOWN;
+  }
 
-  private String getBaPlatformVersion() {
+  private static String getPluginVersion( Bundle bundle ) {
+    try {
+      URL versionFileUrl = bundle.getResource( "version.xml" );
+      InputStream versionFile = versionFileUrl.openConnection().getInputStream();
+      return parsePluginVersion( versionFile );
+    } catch ( NullPointerException npe ) {
+      logger.warn( "Could not find resource file version.xml", npe );
+    } catch ( IOException ioe ) {
+      logger.warn( "Could not open file version.xml", ioe );
+    }
+    return UNKNOWN;
+  }
+
+  private static String parsePluginVersion( InputStream versionFile ) {
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    try {
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      Document doc = db.parse( versionFile );
+      NodeList nodes = doc.getElementsByTagName( "version" );
+      if ( nodes.getLength() >= 1 ) {
+        Element versionElement = (Element) nodes.item( 0 );
+        return versionElement.getAttribute( "branch" ) + "-" + versionElement.getTextContent();
+      }
+    } catch ( Exception e ) {
+      logger.warn( "Could not parse plugin version from version.xml", e );
+    }
+    return UNKNOWN;
+  }
+
+  private static String getBaVersion() {
     String platformVersion;
     VersionInfo versionInfo;
     IVersionHelper versionHelper = PentahoSystem.get( IVersionHelper.class, null );
@@ -64,65 +112,7 @@ public class BaPluginTelemetry extends TelemetryService {
     } else {
       versionInfo = VersionHelper.getVersionInfo( PentahoSystem.class );
     }
-    platformVersion = versionInfo.getProductID() + "_" + versionInfo.getVersionNumber();
+    platformVersion = versionInfo.getProductID() + "-" + versionInfo.getVersionNumber();
     return platformVersion;
   }
-
-  private String getBaPluginVersion( String pluginName ) {
-
-    IApplicationContext context = PentahoSystem.getApplicationContext();
-
-    String versionPath = PentahoSystem.getApplicationContext().getSolutionPath("system/" + pluginName + "/version.xml");
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    FileReader reader = null;
-    try {
-      File file = new File(versionPath);
-      if (!file.exists()) {
-        return "Unknown";
-      }
-      DocumentBuilder db = dbf.newDocumentBuilder();
-      reader = new FileReader(versionPath);
-      Document dom = db.parse(new InputSource(reader));
-      NodeList versionElements = dom.getElementsByTagName("version");
-      if (versionElements.getLength() >= 1) {
-        Element versionElement = (Element) versionElements.item(0);
-        return versionElement.getAttribute("branch") + "-" + versionElement.getTextContent();
-      }
-    } catch (Exception e) {
-      logger.error("Error while trying to read plugin version for " + pluginName, e);
-    } finally {
-      try {
-        if (reader != null) {
-          reader.close();
-        }
-      } catch (Exception e) {
-      }
-    }
-    return "Unknown";
-  }
-
-  // endregion
-
-  /*
-  public boolean isPluginTelemetryEnabled() {
-    return Boolean.parseBoolean( PentahoSystem.getSystemSetting( "telemetry", "true" ) );
-  }
-
-  protected String getTelemetryBaseUrlFromPlugin() {
-    IPluginResourceLoader resLoader = PentahoSystem.get( IPluginResourceLoader.class, null );
-    String baseUrl = null;
-    try {
-      baseUrl = resLoader.getPluginSetting( getClass(), "settings/telemetry-site" ); //$NON-NLS-1$
-    } catch ( Exception e ) {
-      logger.debug( "Error getting data access plugin settings", e );
-    }
-
-    if ( baseUrl == null || "".equals( baseUrl ) ) {
-      logger.warn( "Telemetry url is not set for plugin " + pluginName + ". Defaulting to a bogus local url" );
-      baseUrl = "https://localhost:8080/pentaho/telemetry";
-    }
-
-    return baseUrl;
-  }
-  */
 }

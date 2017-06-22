66750256ba79ae0025e91c4088c0cf635b6c9700
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2017-06-22 17:37:05 +0300

    [PPP-3735] - Use of vulnerable component httpclient-3.0.1.jar, commons-httpclient-3.1 - httpclient-4.0.1.jar, SONATYPE-2007-0004, CVE-2012-6153, CVE-2011-1498, CVE-2014-3577

--- marketplace-core/src/test/java/org/pentaho/telemetry/TelemetryEventSenderTest.java
+++ marketplace-core/src/test/java/org/pentaho/telemetry/TelemetryEventSenderTest.java
@@ -12,298 +12,29 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2015 Pentaho Corporation. All rights reserved.
+ * Copyright (c) 2015-2017 Pentaho Corporation. All rights reserved.
  */
 
 package org.pentaho.telemetry;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnection;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpState;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.StatusLine;
-import org.apache.commons.httpclient.URI;
-import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.auth.AuthState;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 
 public class TelemetryEventSenderTest extends TelemetryBaseTest {
-
-  public class TelemetryEventSenderForTests extends TelemetryEventSender {
-
-    public int blockSize;
-    public String blockDataZero;
-    public String blockDataOne;
-
-    public TelemetryEventSenderForTests( File telemetryDir,
-                                         final int resultCode, final boolean exception ) {
-
-      super( telemetryDir );
-
-      defaultHttpMethod = new PostMethod() {
-
-        private int callNumber;
-
-        @Override
-        public void addParameter( String paramName, String paramValue ) {
-        }
-
-        @Override
-        public void setRequestEntity( RequestEntity requestEntity ) {
-          StringRequestEntity r = (StringRequestEntity) requestEntity;
-          if ( callNumber == 0 ) {
-            blockDataZero = r.getContent();
-          } else {
-            blockDataOne = r.getContent();
-          }
-          callNumber++;
-
-        }
-
-        @Override
-        public void setURI( URI uri ) throws URIException {
-        }
-
-        @Override
-        public void setStrictMode( boolean bln ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean isStrictMode() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setRequestHeader( String string, String string1 ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setRequestHeader( Header header ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void addRequestHeader( String string, String string1 ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void addRequestHeader( Header header ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header getRequestHeader( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void removeRequestHeader( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void removeRequestHeader( Header header ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean getFollowRedirects() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setFollowRedirects( boolean bln ) {
-        }
-
-        @Override
-        public void setQueryString( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setQueryString( NameValuePair[] nvps ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public String getQueryString() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header[] getRequestHeaders() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header[] getRequestHeaders( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean validate() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public int getStatusCode() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public String getStatusText() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header[] getResponseHeaders() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header getResponseHeader( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header[] getResponseHeaders( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header[] getResponseFooters() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public Header getResponseFooter( String string ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public byte[] getResponseBody() throws IOException {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public String getResponseBodyAsString() throws IOException {
-          return "<result>OK</result>";
-        }
-
-        @Override
-        public InputStream getResponseBodyAsStream() throws IOException {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean hasBeenUsed() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public int execute( HttpState hs, HttpConnection hc ) throws HttpException, IOException {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void abort() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void recycle() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void releaseConnection() {
-
-        }
-
-        @Override
-        public void addResponseFooter( Header header ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public StatusLine getStatusLine() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean getDoAuthentication() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setDoAuthentication( boolean bln ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public HttpMethodParams getParams() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void setParams( HttpMethodParams hmp ) {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public AuthState getHostAuthState() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public AuthState getProxyAuthState() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public boolean isRequestSent() {
-          throw new UnsupportedOperationException( "Not supported yet." );
-        }
-      };
-
-      defaultHttpClient = new HttpClient() {
-        @Override
-        public int executeMethod( HttpMethod method ) throws IOException {
-          if ( exception ) {
-            throw new IOException();
-          }
-          return resultCode;
-        }
-      };
-    }
-
-    @Override
-    protected void sendRequest( File[] blockToSend ) {
-      this.blockSize = 0;
-      for ( File f : blockToSend ) {
-        if ( f != null ) {
-          blockSize++;
-        }
-      }
-      super.sendRequest( blockToSend );
-    }
-  }
 
   @Test
   public void TestSend() throws InterruptedException, IOException {
@@ -323,13 +54,30 @@ public class TelemetryEventSenderTest extends TelemetryBaseTest {
     Assert.assertEquals( files.length, 1 );
 
     // activate the event sender
-    TelemetryEventSenderForTests eventSender = new TelemetryEventSenderForTests( telemetryDir.getRoot(), 200, false );
+    TelemetryEventSender eventSender = spy( new TelemetryEventSender( telemetryDir.getRoot() ) );
+    HttpPost methodMock = spy( new HttpPost( TEST_TELEMETRY_URL ) );
+
+    String blockDataTest = "\"pluginName\":\"" + TEST_PLUGIN_NAME + "\"";
+    doAnswer( new Answer<Void>() {
+      @Override
+      public Void answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] arguments = invocation.getArguments();
+        if ( arguments != null && arguments.length > 0 && arguments[ 0 ] != null ) {
+          HttpEntity entity = (HttpEntity) arguments[ 0 ];
+          String result = EntityUtils.toString( entity, "UTF-8" );
+          Assert.assertTrue( result.indexOf( blockDataTest ) > 0 );
+        }
+        return null;
+      }
+    } ).when( methodMock ).setEntity( any( HttpEntity.class ) );
+
+    doReturn( HttpStatus.SC_OK ).when( eventSender ).getStatusCode( any( HttpResponse.class ) );
+    doReturn( "<result>OK</result>" ).when( eventSender ).getResultXml( any( HttpResponse.class ) );
+    eventSender.setDefaultHttpMethod( methodMock );
     eventSender.run();
 
     // block data should have the test plugin name
-    String blockDataTest = "\"pluginName\":\"" + TEST_PLUGIN_NAME + "\"";
-    Assert.assertTrue( eventSender.blockDataZero.indexOf( blockDataTest ) > 0 );
-    Assert.assertEquals( eventSender.blockSize, 2 );
+    verify( eventSender, atLeast( 1 ) ).sendRequest( any( File[].class ) );
 
     // telemetry dir should be empty
     files = getFilesInTelemetryDir();
